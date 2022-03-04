7beb2d09bcaf4e2123516d9b8b3e1afa08ef9ac0
Donadams50 <sumbomatic@gmail.com>
2022-03-04 13:30:22 +0100

    update?

--- selenium_test.js
+++ selenium_test.js
@@ -10,26 +10,29 @@ const driver = new webdriver.Builder()
     .forBrowser('chrome')
     .build();
 
- driver.get('https://bulk.astrapay.com.ng').then(function(){
-  username= driver.findElement(By.id("username"));
-  password= driver.findElement(By.id("password"));
-  login= driver.findElement(By.id("signIn"));
-  username.sendKeys(process.env.username);
-  password.sendKeys(process.env.password);
-  login.click();
 
-   actualUrl=driver.getCurrentUrl();
 
-   console.log(actualUrl)
-// driver.findElement(webdriver.By.name('q')).sendKeys('webdriver\n').then(function(){
-//     driver.getTitle().then(function(title) {
-//       console.log(title)
-//       if(title === 'webdriver - Google Search') {
-//          console.log('Test passed');
-//       } else {
-//          console.log('Test failed');
-//       }
-//      //driver.quit();
-//     });
-//   });
-});
+
+const postDeviceToken = async()=>{
+            try{
+                await  driver.get('https://bulk.astrapay.com.ng')
+                username= await driver.findElement(By.id("username"));
+                password=  await driver.findElement(By.id("password"));
+                login= await driver.findElement(By.id("signIn"));
+                await username.sendKeys(process.env.username);
+                await password.sendKeys(process.env.password);
+                await  login.click();
+                
+                elementval = await  driver.findElement(By.id("groupCount")).getAttribute("value");
+                console.log(elementval)
+            }catch(err){
+                console.log(err)      
+            }
+ }
+
+ postDeviceToken()
+            
+
+
+
+  
